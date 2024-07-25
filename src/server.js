033c61c6238db7fb8bed52c31d355aeab8e2b217
Christianto Kurniawan <christiantokape@gmail.com>
2024-07-25 23:16:21 +0700

    saving albums

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const AlbumsService = require('./services/postgres/AlbumsService');
+const AlbumsValidator = require('./validator/albums');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -11,6 +16,31 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+      validator: AlbumsValidator,
+    },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    // penanganan client error secara internal
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
   });
 
   await server.start();
