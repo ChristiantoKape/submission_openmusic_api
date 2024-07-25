2f720886d1a264c1f9a6ece7dc5223face2304ee
Christianto Kurniawan <christiantokape@gmail.com>
2024-07-26 03:15:19 +0700

    change song

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -6,6 +6,7 @@ class SongHandler {
     this.postSongHandler = this.postSongHandler.bind(this);
     this.getSongsHandler = this.getSongsHandler.bind(this);
     this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+    this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
   }
 
   async postSongHandler(request, h) {
@@ -53,6 +54,18 @@ class SongHandler {
       data: {
         song,
       },
+    };
+  }
+
+  async putSongByIdHandler(request) {
+    this._validator.validateSongPayload(request.payload);
+    const { id } = request.params;
+
+    await this._service.editSongById(id, request.payload);
+
+    return {
+      status: 'success',
+      message: 'Lagu berhasil diperbarui',
     };
   }
 }
